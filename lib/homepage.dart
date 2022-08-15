e852b1303e674315003f3b06b68651eb9fccafd9
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-08-15 11:31:05 -0300

    ListView

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -42,28 +42,28 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   var counter = 0;
+  final nomes = ['Luciano','Ketlen','Ana','Arthur','Cris','Lucas','Jorge','Daniel','Marcia','Amanda'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("curso"),),
         body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                for(int i = 0; i < 20; i++)
-                  Container(
-                    height: 80,
-                    width: double.infinity,
-                    margin: EdgeInsets.all(20),
-                    color: Colors.green,
+          child: ListView.builder(
+              itemCount: nomes.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 50,
+                  width: double.infinity,
+                  margin: EdgeInsets.all(20),
+                  color: Colors.yellowAccent,
+                  child: ListTile(
+                    title: Text(nomes[index])
                   ),
-              ],
+                );
+              }
             ),
           )
-        )
-    );
+        );
   }
 }
