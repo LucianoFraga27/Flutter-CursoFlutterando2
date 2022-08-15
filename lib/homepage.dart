03095d65485778e9fa1769e9266537f17b1ffb53
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-08-15 11:43:09 -0300

    ex prático de columns,rows, listview

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -41,29 +41,46 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  var counter = 0;
-  final nomes = ['Luciano','Ketlen','Ana','Arthur','Cris','Lucas','Jorge','Daniel','Marcia','Amanda'];
+  final controller = TextEditingController();
+
+  final lista = <String>[];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("curso"),),
-        body: Center(
-          child: ListView.builder(
-              itemCount: nomes.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  height: 50,
-                  width: double.infinity,
-                  margin: EdgeInsets.all(20),
-                  color: Colors.yellowAccent,
-                  child: ListTile(
-                    title: Text(nomes[index])
-                  ),
-                );
-              }
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: [
+                  Expanded(child: TextField(controller: controller,)),
+                  IconButton(onPressed: (){
+                    final texto = controller.text;
+                    setState(
+                        () {
+                          lista.add(texto);
+                        }
+                    );
+                    controller.clear();
+                  }, icon: const Icon(Icons.add))
+                ],
+              ),
             ),
-          )
+            Expanded(
+              child: ListView.builder(
+                  itemCount: lista.length,
+                  itemBuilder: (context, index){
+                    final item = lista[index];
+                    return ListTile(
+                      title: Text(item),
+                    );
+                  }
+              ),
+            )
+          ],
+        )
         );
   }
 }
